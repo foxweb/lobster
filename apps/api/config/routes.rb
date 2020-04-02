2ecf6b91ab42de0c6c2ec15a08c7d267c26dcc8a
Aleksey Kurepin <lesha@kurepin.com>
2020-04-02 18:07:30 +0300

    routes updated

--- apps/api/config/routes.rb
+++ apps/api/config/routes.rb
@@ -1,1 +1,10 @@
 get '/healthcheck', to: ->(_) { [200, {}, ['Hello from Lobster!']] }
+
+post '/authenticate', to: 'sessions#authenticate'
+get  '/new_token',    to: 'sessions#refresh'
+
+resources :images, only: %i[] do
+  collection do
+    post :upload
+  end
+end
